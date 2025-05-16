e724172eb8437ae8a6b37fd7a902e7c8f6d44702
zhirongfu <zhi.fu57@myhunter.cuny.edu>
2025-05-15 22:51:42 -0400

    new file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import "./App.css";
-
-//Router
 import { Switch, Route } from "react-router-dom";
-//Components
 import {
   HomePageContainer,
   CampusContainer,
   StudentContainer,
   AllCampusesContainer,
   AllStudentsContainer,
-  NewStudentContainer
+  NewStudentContainer,
+  AddCampusContainer
 } from './components/containers';
-
-// if you create separate components for adding/editing 
-// a student or campus, make sure you add routes to those
-// components here
 
 const App = () => {
   return (
@@ -22,6 +16,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePageContainer} />
         <Route exact path="/campuses" component={AllCampusesContainer} />
+        <Route exact path="/campuses/add" component={AddCampusContainer} />
         <Route exact path="/campus/:id" component={CampusContainer} />
         <Route exact path="/students" component={AllStudentsContainer} />
         <Route exact path="/newstudent" component={NewStudentContainer} />
